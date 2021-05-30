0a27c520239a4962d02289f71fe7afb41eb0e9e4
Laba Subagia <labasubagia22@gmail.com>
2021-05-30 18:59:46 +0800

    [add] default user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Brand;
+use App\Models\ProductType;
+use App\Models\Store;
+use App\Models\User;
+use App\Models\Vendor;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,5 +19,22 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call('UsersTableSeeder');
+        $this->makeUser();
+    }
+
+    private function makeUser()
+    {
+        $stores = Store::factory()->count(2)->create();
+        User::factory(['email' => 'retail1@mail.com', 'type' => 'admin_retail', 'store_id' => null])->create();
+        User::factory(['email' => 'customer1@mail.com', 'type' => 'customer', 'store_id' => null])->create();
+        $stores->each(function ($store, $index) {
+            $index += 1;
+            $payload = [
+                'email' => "store{$index}@mail.com",
+                'type' => 'admin_store',
+                'store_id' => $store->id,
+            ];
+            User::factory($payload)->create();
+        });
     }
 }
